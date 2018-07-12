f1a810196ac76eaba1f6239716fd8c64b461bf73
lixingcong <lixingcong@live.com>
2018-07-12 17:14:42 +0800

    minivtun: add IPv6 option

--- files/luci/model/cbi/minivtun.lua
+++ files/luci/model/cbi/minivtun.lua
@@ -15,7 +15,7 @@ s = m:section(TypedSection, "minivtun", translate("General Setting"))
 s.anonymous = true
 
 o = s:option(Flag, "enable", translate("Enable"))
-o.default = 1
+o.default = 0
 o.rmempty = false
 
 o = s:option(Value, "server", translate("Server Address"))
@@ -30,26 +30,35 @@ o = s:option(Value, "password", translate("Password"))
 o.password = true
 o.rmempty = false
 
-o = s:option(Value, "net", translate("Local Subnet"))
-o.placeholder = "10.7.0.2/24"
-o.default = "10.7.0.2/24"
+o = s:option(ListValue, "local_family", translate("Local Subnet Family"))
+o:value("IPv4", translate("IPv4"))
+o:value("IPv6", translate("IPv6"))
 o.rmempty = false
 
+o = s:option(Value, "local_ipaddr4", translate("Local IPv4 Address"))
+o:depends("local_family","IPv4")
+o.datatype = "ip4addr"
+o.default = "10.0.7.2/24"
+o.rmempty = true
+
+o = s:option(Value, "local_ipaddr6", translate("Local IPv6 Address"))
+o:depends("local_family","IPv6")
+o.datatype = "ip6addr"
+o.default = "fc00::2/64"
+o.rmempty = true
+
 o = s:option(Value, "mtu", translate("Override MTU"))
-o.placeholder = 1300
-o.default = 1300
+o.placeholder = 1400
 o.datatype = "range(296,1500)"
 o.rmempty = false
 
 o = s:option(Value, "keepalive", translate("Heart-Beats time"))
-o.placeholder = 13
-o.default = 13
+o.placeholder = 20
 o.datatype = "range(10,30)"
 o.rmempty = false
 
 o = s:option(Value, "intf", translate("Interface Name"))
 o.placeholder = "mv0"
-o.default = "mv0"
 o.rmempty = false
 
 return m
